ba98fb0f07003171650f0dac15dd69fdd163a42d
mar-tchelo <marcelo.js10@gmail.com>
2022-05-15 16:21:06 -0300

    Find movies

---
+++ backend/src/main/java/com/msjmovie/dsmovie/repositories/MovieRepository.java
@@ -0,0 +1,9 @@
+package com.msjmovie.dsmovie.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.msjmovie.dsmovie.entities.Movie;
+
+public interface MovieRepository extends JpaRepository<Movie, Long>  {
+	
+}
